1b2c582b1c59d29af61587607e845386383157c2
Kartik Chokkavarapu <amudda73@gmail.com>
2020-01-11 12:02:12 -0500

    Arjun, Kartik, Chiao worked on wheelRevolutionCounter

--- team612/src/main/java/frc/robot/commands/wheel/ColorWheelCounter.java
+++ team612/src/main/java/frc/robot/commands/wheel/ColorWheelCounter.java
@@ -1,65 +1,56 @@
 /*----------------------------------------------------------------------------*/
-/* Copyright (c) 2018 FIRST. All Rights Reserved.                             */
+/* Copyright (c) 2019 FIRST. All Rights Reserved.                             */
 /* Open Source Software - may be modified and shared by FRC teams. The code   */
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
 
-package frc.robot.commands;
+package frc.robot.commands.wheel;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj2.command.CommandBase;
 
-public class ColorWheelCounter extends Command {
+public class colorWheelCounter extends CommandBase {
 
-  private String intial_color;
+  private String initial_color;
   private String current_color;
   private double revolution_count;
   private String target_color;
-  
-  public ColorWheelCounter() {
-    // Use requires() here to declare subsystem dependencies
-    // eg. requires(chassis);
+  /**
+   * Creates a new WheelRotate.
+   */
+  public colorWheelCounter() {
+    // Use addRequirements() here to declare subsystem dependencies.
   }
 
-  // Called just before this Command runs the first time
+  // Called when the command is initially scheduled.
   @Override
-  protected void initialize() {
-    inital_color = "red";
+  public void initialize() {
+    initial_color = "red";
     target_color = "green";
   }
 
-  // Called repeatedly when this Command is scheduled to run
+  // Called every time the scheduler runs while the command is scheduled.
   @Override
-  protected void execute() {
+  public void execute() {
     current_color = "red";
 
-    if(current_color.equals(inital_color)){
+    if(current_color.equals(initial_color)){
      revolution_count += 0.5;
     }
 
     if(revolution_count >= 3.0 && revolution_count <= 5.0){
       //stop boi
     }
-
-
-    //if current_color is deteced then set intial_color = current_color
-
   }
 
-  // Make this return true when this Command no longer needs to run execute()
+  // Called once the command ends or is interrupted.
   @Override
-  protected boolean isFinished() {
+  public void end(boolean interrupted) {
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
     return false;
-  }
-
-  // Called once after isFinished returns true
-  @Override
-  protected void end() {
-  }
-
-  // Called when another command which requires one or more of the same
-  // subsystems is scheduled to run
-  @Override
-  protected void interrupted() {
   }
 }
