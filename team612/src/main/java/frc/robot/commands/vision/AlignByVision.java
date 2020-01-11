25cfba4bd53564764edc18971c63f4817c6de845
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-01-11 08:44:30 -0500

    Tweaked functions so it runs, need to tune PID values next

--- team612/src/main/java/frc/robot/commands/vision/AlignByVision.java
+++ team612/src/main/java/frc/robot/commands/vision/AlignByVision.java
@@ -70,52 +70,56 @@ public class AlignByVision extends CommandBase {
     https://readthedocs.org/projects/limelight/downloads/pdf/latest/
     Chapter 11: Aiming and Range at the same time.
     */
-
-    tx = listener.getOffset();  // Get current offset reading from camera
-
-    // Distance value
-    distance = m_drivetrain.ultrasonic_drive.getRangeInches();
-    distance_error = distance * kp_d;
-
-    // Start accumulating offset if offset is stuck at low value
-    if (tx < integral_limit) {
-      tx_sum += tx;
-    } else {
-      tx_sum = 0;
-    }
-
-    // Limit integral sum from getting to large
-    tx_sum = tx_sum > 50/ki ? 50/ki : tx_sum;
-
-    // If the offset is at 0 reset set D and I to 0
-    if (tx == 0) {
-      derivative = 0;
-      tx_sum = 0;
-    }
-
-    // PID calculations
-    proportion = tx * kp;
-    derivative = (tx-tx_prev) * kd;
-    integral = tx_sum * ki;
-
-    tx_prev = tx;  // Assign the previous motor value for derivative
-
-    throttle = proportion + integral + derivative;
-    throttle = throttle > 1 ? 1 : throttle < -1 ? -1 : throttle;  // Truncate throttle value between -1 and 1
-
-    // TODO: Normally += throttle to PID on distance for left and right command
-    left_command = throttle + distance_error;
-    right_command = throttle - distance_error;
-
-    // Basic print statements and tank drive apply
-    System.out.println(throttle);
-    System.out.println(distance_error);
-    System.out.println(left_command + " | " + right_command);
     System.out.println(listener.isTargetFound());
-    m_drivetrain.tank_drive(left_command, right_command);
+    if (listener.isTargetFound()) {
+      tx = listener.getOffset();  // Get current offset reading from camera
 
-    if (tx < tx_tolerance && distance < distance_tolerance) {
-      ALIGNED = true;
+      // Distance value
+      distance = m_drivetrain.ultrasonic_drive.getRangeInches();
+      distance_error = distance * kp_d;
+
+      // Start accumulating offset if offset is stuck at low value
+      if (tx < integral_limit) {
+        tx_sum += tx;
+      } else {
+        tx_sum = 0;
+      }
+
+      // Limit integral sum from getting to large
+      tx_sum = tx_sum > 50/ki ? 50/ki : tx_sum;
+
+      // If the offset is at 0 reset set D and I to 0
+      if (tx == 0) {
+        derivative = 0;
+        tx_sum = 0;
+      }
+
+      // PID calculations
+      proportion = tx * kp;
+      derivative = (tx-tx_prev) * kd;
+      integral = tx_sum * ki;
+
+      tx_prev = tx;  // Assign the previous motor value for derivative
+
+      throttle = proportion + integral + derivative;
+      throttle = throttle > 1 ? 1 : throttle < -1 ? -1 : throttle;  // Truncate throttle value between -1 and 1
+
+      // TODO: Normally += throttle to PID on distance for left and right command
+      left_command = throttle + distance_error;
+      right_command = throttle - distance_error;
+
+      // Basic print statements and tank drive apply
+      System.out.println(throttle);
+      System.out.println(distance_error);
+      System.out.println(left_command + " | " + right_command);
+      System.out.println(listener.isTargetFound());
+      m_drivetrain.tank_drive(left_command, right_command);
+
+      if (tx < tx_tolerance && distance < distance_tolerance) {
+        ALIGNED = true;
+      }
+    } else {
+      m_drivetrain.tank_drive(0, 0);
     }
 
   }
@@ -124,6 +128,7 @@ public class AlignByVision extends CommandBase {
   @Override
   public void end(boolean interrupted) {
     System.out.println("Aligned!");
+    m_drivetrain.tank_drive(0, 0);
   }
 
 
