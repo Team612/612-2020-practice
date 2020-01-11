58e330d9eb2d3194b5d299194bc39a8042b9463d
Kai Fergerstrom <kaifergerstrom@gmail.com>
2020-01-11 11:23:38 -0500

    Created high/low gear and ultrasonic

--- team612/src/main/java/frc/robot/subsystems/Drivetrain.java
+++ team612/src/main/java/frc/robot/subsystems/Drivetrain.java
@@ -9,6 +9,9 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.DoubleSolenoid;
+import edu.wpi.first.wpilibj.Ultrasonic;
+import edu.wpi.first.wpilibj.DoubleSolenoid.Value;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
@@ -19,10 +22,16 @@ private WPI_TalonSRX talon_fl_drive = new WPI_TalonSRX(Constants.talon_fl_port);
 private WPI_TalonSRX talon_br_drive = new WPI_TalonSRX(Constants.talon_br_port);
 private WPI_TalonSRX talon_bl_drive = new WPI_TalonSRX(Constants.talon_bl_port);
 
+//Ultrasonic 
+private Ultrasonic ultra = new Ultrasonic(Constants.ultrasonic_ping_port, Constants.ultrasonic_echo_port);
+
+private DoubleSolenoid solenoid_drive = new DoubleSolenoid(Constants.solenoid_forward_channel, Constants.solenoid_reverse_channel);
+
 public void westCoastDrive(double leftCommand, double rightCommand, double deadzone){  
-  
+
+  //sets up deadzones
   leftCommand = leftCommand < deadzone ? 0.0 : leftCommand;
-  //west coast drive logic
+  rightCommand = rightCommand < deadzone ? 0.0 : rightCommand;
 
   //right side motor controls
   talon_fr_drive.set(rightCommand);
@@ -32,12 +41,25 @@ public void westCoastDrive(double leftCommand, double rightCommand, double deadz
   talon_fl_drive.set(leftCommand);
   talon_bl_drive.set(leftCommand);
 
-
 }
 
+public double getDistance() {
+  return ultra.getRangeInches();
+}
+
+public void shiftForward(){
+  solenoid_drive.set(Value.kForward);
+  solenoid_drive.set(Value.kOff);
+  
+}
+public void shiftReverse(){
+  solenoid_drive.set(Value.kReverse);
+  solenoid_drive.set(Value.kOff);
+}
   public Drivetrain() {
-    
-  }
+    ultra.setEnabled(true);
+    ultra.setAutomaticMode(true);
+ }
 
   @Override
   public void periodic() {
