644c17b34e15837673f06240b07031bfb4bb5294
Kartik Chokkavarapu <amudda73@gmail.com>
2020-01-13 20:23:48 -0500

    Kartick, Emile, Nitin, Chaio, Arjun,  attampeted to finish color wheel

--- team612/src/main/java/frc/robot/commands/drivetrain/DefaultDrive.java
+++ team612/src/main/java/frc/robot/commands/drivetrain/DefaultDrive.java
@@ -6,11 +6,7 @@ import edu.wpi.first.wpilibj2.command.CommandBase;
 
 public class DefaultDrive extends CommandBase {
 
-  @SuppressWarnings({"PMD.UnusedPrivateField", "PMD.SingularField"})
-  private final Drivetrain m_drivetrain;
-
   public DefaultDrive(Drivetrain m_drivetrain) {
-    this.m_drivetrain = m_drivetrain;
     addRequirements(m_drivetrain);
   }
 
--- team612/src/main/java/frc/robot/commands/wheel/SpinToColor.java
+++ team612/src/main/java/frc/robot/commands/wheel/SpinToColor.java
@@ -1,11 +1,23 @@
 package frc.robot.commands.wheel;
 
+
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Wheel;
 
-public class SpinToColor extends CommandBase {
 
+public class SpinToColor extends CommandBase {
+  private int arrayValue(char[] arrayName){
+    int a = -1;
+    for (int i = 0; i < arrayName.length; i++ ){
+        if (targetColor == colorPattern[i]){
+          return i; 
+        }
+      }
+      return a;
+    }
+    
+  
   char[] colorPattern = {'R', 'Y', 'B', 'G'};  // Pattern of wheel colors
   
   int offset = 2;  // Color offset (actual vs. target)
@@ -21,59 +33,41 @@ public class SpinToColor extends CommandBase {
   public SpinToColor(Wheel m_wheel) {
     this.m_wheel = m_wheel;
     addRequirements(m_wheel); // needed?
+
+    targetColor = DriverStation.getInstance().getGameSpecificMessage().charAt(0);
+
+    int index = (arrayValue(colorPattern) + 2) % 4;
+    sensorTarget = colorPattern[index];
   }
 
   @Override
   public void initialize() {
-    isComplete = false;  // Reset isComplete each call
-    if (DriverStation.getInstance().getGameSpecificMessage().length() > 0) {
-      targetColor = DriverStation.getInstance().getGameSpecificMessage().charAt(0);  // Actual target value
-      sensorTarget = getScaledTargetColor(targetColor);  // Scaled target value 2 more steps into the pattern
-    } else {
-      end(true);  // End the function right away if not wheel stage
-    }
   }
 
   @Override
   public void execute() {
-
+    
     currentColor = m_wheel.getClosestColor();  // Current sensor reading updating each loop
-
-    // End the command once target value is hit
-    if (currentColor == sensorTarget) {
-      isComplete = true;
+    if(currentColor != sensorTarget){
+      //motor move at power x
+      System.out.println("motor move x");
+    }
+    if (currentColor == sensorTarget){
+      //Stop motor
+      System.out.println("STOP!!!");
     }
 
-    // TODO: Rotate at fixed rate
 
   }
 
   @Override
   public void end(boolean interrupted) {
-    // TODO: Stop the motor
+    // TODO: Stop the m2otor
   }
 
   @Override
   public boolean isFinished() {
     return isComplete;
-  }
-
-  /* ------ Custom Functions ------ */
-
-  // Returns index of char in array
-  private int getIndex(char target) {
-    for (int i = 0; i < colorPattern.length; ++i) {
-      if (colorPattern[i] == target) {
-        return i;
-      }
-    }
-    return -1;
-  }
-
-  // Gets char of pattern two steps from target value
-  private char getScaledTargetColor(char color) {
-    int targetIndex = (getIndex(color) + 2) % 4;
-    return colorPattern[targetIndex];
   }
   
 }
