22963900c45e002894b5a460bf97f28599f004fa
Srihan21 <32943772+Srihan21@users.noreply.github.com>
2020-01-14 20:28:27 -0500

    Update DefaultDriveAssist.java

--- team612/src/main/java/frc/robot/commands/drivetrain/DefaultDriveAssist.java
+++ team612/src/main/java/frc/robot/commands/drivetrain/DefaultDriveAssist.java
@@ -25,6 +25,9 @@ public class DefaultDriveAssist extends CommandBase {
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
+     if(ControlMap.driver_button_RJ.get()){
+      m_drivetrain.drivePID();
+    }
   }
 
   // Called once the command ends or is interrupted.
