d0a1fd395ceeb4bf591137e0c60e03ecf3bf7499
Srihan21 <32943772+Srihan21@users.noreply.github.com>
2020-01-20 18:56:48 -0500

    update accel calculations

--- team612/src/main/java/frc/robot/commands/drivetrain/Velocity.java
+++ team612/src/main/java/frc/robot/commands/drivetrain/Velocity.java
@@ -7,27 +7,40 @@
 
 package frc.robot.commands.drivetrain;
 
+import edu.wpi.first.wpilibj.SPI;
 import edu.wpi.first.wpilibj.Timer;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import com.analog.adis16448.frc.ADIS16448_IMU;
 
 public class Velocity extends CommandBase {
-  private double currentTimer, initialTimer;
-  ADIS16448_IMU accel = new ADIS16448_IMU(/*port*/ );
-  //Accelerometer accelerometer = new Accelerometer(X);
+  
+  private double initialTimer = 0;
+  private double currentTimer;
 
-  private double currentAccelX, initialAccelX; 
-  private double currentAccelY, initialAccelY;
-  private double currentAccelZ, initialAccelZ;
+  private double currentAccelX;
+  private double currentAccelY;
+  private double currentAccelZ;
+  //private double[] currentAccelXYZ = {0, 0, 0}; //might want to use arrays
+  private double initialAccelX; 
+  private double initialAccelY; 
+  private double initialAccelZ; 4
 
-  private double currentVelocityZ = 0;
-  private double currentVelocityY = 0;
-  private double currentVelocityX = 0; 
+  private double currentVelocityX;
+  private double currentVelocityY;
+  private double currentVelocityZ; 
+
+  private double initialVelocityX = 0;
+  private double initialVelocityY = 0;
+  private double initialVelocityZ = 0; 
+
+  private double displacementX = 0; 
+  private double displacementY = 0; 
+  private double displacementZ = 0; 
 
   Timer timer = new Timer(); 
 
-  
+  ADIS16448_IMU accel = new ADIS16448_IMU();
 
   public Velocity() {
     // Use addRequirements() here to declare subsystem dependencies.
@@ -36,36 +49,33 @@ public class Velocity extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    
+    initialAccelX = 0;//might want to init in initialize 
+    initialAccelY = 0;
+    initialAccelZ = 0;
+    //currentAccelXYZ[0] = 0;
+    //currentAccelXYZ[1] = 0;
+    //currentAccelXYZ[2] = 0;
+    timer.start();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
   public void execute() {
-    timer.start();
-    System.out.println(timer.get());
-    timer.reset();
+    timer.reset(); // reset timer for next iteration of summation. Sets time = 0.
+ 
+    //System.out.println(accel.getAccelInstantX());
+    //System.out.println(accel.getAccelInstantY());
+    //System.out.println(accel.getAccelInstantZ());
 
-    System.out.println(accel.getAccelInstantX());
-    System.out.println(accel.getAccelInstantY());
-    System.out.println(accel.getAccelInstantZ());
-
-    currentAccelX = accel.getAccelInstantX();
-    currentAccelY = accel.getAccelInstantY();
-    currentAccelZ = accel.getAccelInstantZ();
-
-
-    currentVelocityX = ((initialAccelX + currentAccelX)/2.0)*(currentTimer - initialTimer);
-    currentVelocityY = ((initialAccelY + currentAccelY)/2.0)*(currentTimer - initialTimer);
-    currentVelocityZ = ((initialAccelZ + currentAccelZ)/2.0)*(currentTimer - initialTimer);
-
-
-    initialAccelZ = currentAccelZ; 
-    initialAccelY = currentAccelY; 
-    initialAccelX = currentAccelX; 
-
-    initialTimer = currentTimer; 
+    getAccelerometer(); // Capture 3-Axis accelerometer values.
+    getTimer();  // Capture timer value
     
+
+    trapezoidalReimannX(); // Trapezoidal Reimann sum for X-Axis displacement
+    trapezoidalReimannY(); // Trapezoidal Reimann sum for Y-Axis displacement
+    trapezoidalReimannZ(); // Trapezoidal Reimann sum for Z-Axis displacement
+
+    updateValues(); // Update calculation values for next iteration of summation. Sets initial value to current value. 
     
 
   }
@@ -80,5 +90,65 @@ public class Velocity extends CommandBase {
   @Override
   public boolean isFinished() {
     return false;
+  }
+/*
+  private void trapezoidalReimann(double vel, double ia, double ca, double ct, double it) { // 
+    vel+""
+
+    vel = this.initial + ((ia + ca)/2.0)*(ct - it);
+  }*/
+
+  private void getAccelerometer(){
+    currentAccelX = accel.getAccelInstantX();
+    currentAccelY = accel.getAccelInstantY();
+    currentAccelZ = accel.getAccelInstantZ();
+  }
+
+  private void getTimer(){
+    currentTimer = timer.get();
+  }
+
+  private void trapezoidalReimannX(){
+      currentVelocityX = initialVelocityX + ((initialAccelX + currentAccelX)/2.0)*(currentTimer - initialTimer);
+
+      displacementX = displacementX + ((initialVelocityX + currentVelocityX)/2.0)*(currentTimer - initialTimer);
+  }
+
+  private void trapezoidalReimannY(){
+      currentVelocityY = initialVelocityY + ((initialAccelY + currentAccelY)/2.0)*(currentTimer - initialTimer);
+      displacementY = displacementY + ((initialVelocityY + currentVelocityY)/2.0)*(currentTimer - initialTimer);
+
+  }
+
+  private void trapezoidalReimannZ(){
+      currentVelocityZ = initialVelocityZ + ((initialAccelZ + currentAccelZ)/2.0)*(currentTimer - initialTimer);
+      displacementZ = displacementZ + ((initialVelocityZ + currentVelocityZ)/2.0)*(currentTimer - initialTimer);
+
+  }
+
+  private void midpointReimannX(){
+    //figure out a way to find accelorometer value at midpoint of time readings
+    //1, store all accelerometer values and then do midpoints from that
+    //2, gradually create regression function that can approximate midpoint, big brain
+    //4, or just average right left and trapezoidal
+    //midpoint reimann averaged with trapezoidal will give us the best result
+  }
+
+  private void midpointReimannY(){
+
+  }
+
+  private void midpointReimannZ(){
+
+  }
+
+  private void updateValues(){
+    initialAccelZ = currentAccelZ; 
+    initialAccelY = currentAccelY; 
+    initialAccelX = currentAccelX; 
+    initialVelocityX = currentVelocityX;
+    initialVelocityY = currentVelocityY;
+    initialVelocityZ = currentVelocityZ;
+    initialTimer  = currentTimer; 
   }
 }
