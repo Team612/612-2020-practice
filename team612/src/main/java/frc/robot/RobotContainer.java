673095086396316d21797323416447d120ad1cc8
unknown <anoopdavid@outlook.com>
2020-01-25 16:51:17 -0500

    fixed issues with accelerometer code. Will have it working perfectly by Monday's meeting. -Jacob

--- team612/src/main/java/frc/robot/RobotContainer.java
+++ team612/src/main/java/frc/robot/RobotContainer.java
@@ -3,13 +3,16 @@ package frc.robot;
 import edu.wpi.first.wpilibj2.command.Command;
 import frc.robot.commands.autonomous.SampleAuto;
 import frc.robot.commands.drivetrain.DefaultDrive;
+import frc.robot.commands.drivetrain.DefaultVelocity;
 import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Velocity;
 
 public class RobotContainer {
 
   private final Drivetrain m_drivetrain = new Drivetrain();
+  private final Velocity m_velocity = new Velocity();
   private final DefaultDrive c_defaultdrive = new DefaultDrive(m_drivetrain);
-
+  private final DefaultVelocity c_velocity = new DefaultVelocity(m_velocity);
   private final SampleAuto m_sampleauto = new SampleAuto();
 
   public RobotContainer() {
@@ -25,6 +28,7 @@ public class RobotContainer {
   // Put all default commands here
   private void configureDefaultCommands() {
     m_drivetrain.setDefaultCommand(c_defaultdrive);
+    m_velocity.setDefaultCommand(c_velocity);
   }
 
   public Command getAutonomousCommand() {
