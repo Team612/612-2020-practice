090e10bd2cf150503e2feffd49e207b2074b5b9e
unknown <anoopdavid@outlook.com>
2020-02-04 08:58:42 -0500

    fixed talon_br_port value

--- team612/src/main/java/frc/robot/Constants.java
+++ team612/src/main/java/frc/robot/Constants.java
@@ -5,7 +5,7 @@ public final class Constants {
     // Drivetrain talon ports
     public static int talon_fr_port = 0;
     public static int talon_fl_port = 14;
-    public static int talon_br_port = 13;
+    public static int talon_br_port = 1;
     public static int talon_bl_port = 15;
 
     // Forward and reverse channel for drive double solenoid
